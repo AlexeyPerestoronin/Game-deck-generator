373cc65b771310418202ec90b7487a72170b21c3
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2026-09-20 18:48:48 +0600

    implement harness: iteration-5

--- harness/__init__.py
+++ harness/__init__.py
@@ -1,10 +1,11 @@
 import invoke
+import pathlib
 
 from . import (tools, agent, logger)
 
 @invoke.task()
-def run_agent(ctx, prompt: str, iteration_limit: int = 25):
-    logger = logger.DoubleLogger()
+def run_agent(ctx, prompt: str, log_file: str, iteration_limit: int = 25):
+    logger = logger.DoubleLogger(pathlib.Path(log_file))
     ai_agent = agent.Grok(prompt, tools.DefaultToolsSet(), logger)
     agent_loop = agent.AgentLoop(iteration_limit, ai_agent, logger)
     agent_loop.start_loop()
